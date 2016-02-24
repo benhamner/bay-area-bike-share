fd9476abee87b60767fb7c19bd03310a88ae4675
Ben Hamner <ben@benhamner.com>
2016-02-24 10:18:08 -0700

    initial commit

---
+++ Makefile
@@ -0,0 +1,55 @@
+unzip:
+	cd input; unzip \*.zip
+	cd input/2015; unzip \*.zip
+
+output/weather.csv:
+	mkdir -p output
+	python src/process.py
+output/station.csv: output/weather.csv
+output/status.csv: output/weather.csv
+output/trip.csv: output/weather.csv
+
+working/no_header/station.csv: output/station.csv
+	mkdir -p working/no_header
+	tail +2 $^ > $@
+
+working/no_header/status.csv: output/status.csv
+	mkdir -p working/no_header
+	tail +2 $^ > $@
+
+working/no_header/trip.csv: output/trip.csv
+	mkdir -p working/no_header
+	tail +2 $^ > $@
+
+working/no_header/weather.csv: output/weather.csv
+	mkdir -p working/no_header
+	tail +2 $^ > $@
+
+output/database.sqlite: working/no_header/station.csv working/no_header/status.csv working/no_header/trip.csv working/no_header/weather.csv
+	-rm output/database.sqlite
+	sqlite3 -echo $@ < src/import.sql
+db: output/database.sqlite
+
+output/hashes.txt: output/database.sqlite
+	-rm output/hashes.txt
+	echo "Current git commit:" >> output/hashes.txt
+	git rev-parse HEAD >> output/hashes.txt
+	echo "\nCurrent input/ouput md5 hashes:" >> output/hashes.txt
+	md5 output/*.csv >> output/hashes.txt
+	md5 output/*.sqlite >> output/hashes.txt
+	md5 input/*.zip >> output/hashes.txt
+	md5 input/201402_babs_open_data/* >> output/hashes.txt
+	md5 input/201408_babs_open_data/* >> output/hashes.txt
+	md5 input/2015/* >> output/hashes.txt
+hashes: output/hashes.txt
+
+release: output/hashes.txt
+	cp -r output bay-area-bike-share
+	zip -r -X output/bay-area-bike-share-`date -u +'%Y-%m-%d-%H-%M-%S'` bay-area-bike-share/*
+	rm -rf bay-area-bike-share
+
+all: release
+
+clean:
+	rm -rf working
+	rm -rf output
---
+++ src/process.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+station = pd.read_csv("input/2015/201508_station_data.csv")
+station.columns = ["id", "name", "lat", "long", "dock_count", "city", "installation_date"]
+station.to_csv("output/station.csv", index=False)
+
+status_2014_1 = pd.read_csv("input/201402_babs_open_data/201402_status_data.csv")
+status_2014_2 = pd.read_csv("input/201408_babs_open_data/201408_status_data.csv")
+status_2015   = pd.read_csv("input/2015/201508_status_data.csv")
+
+status_columns = ["station_id", "bikes_available", "docks_available", "time"]
+status_2014_1.columns = status_columns
+status_2014_2.columns = status_columns
+status_2015.columns   = status_columns
+
+status = pd.concat([status_2014_1, status_2014_2, status_2015])
+status.to_csv("output/status.csv", index=False)
+
+trip_2014_1 = pd.read_csv("input/201402_babs_open_data/201402_trip_data.csv")
+trip_2014_2 = pd.read_csv("input/201408_babs_open_data/201408_trip_data.csv")
+trip_2015   = pd.read_csv("input/2015/201508_trip_data.csv")
+
+trip_columns = ["id", "duration", "start_date", "start_station_name", "start_station_id", "end_date", "end_station_name", "end_station_id", "bike_id", "subscription_type", "zip_code"]
+trip_2014_1.columns = trip_columns
+trip_2014_2.columns = trip_columns
+trip_2015.columns   = trip_columns
+
+weather = pd.concat([trip_2014_1, trip_2014_2, trip_2015])
+weather.to_csv("output/trip.csv", index=False)
+
+weather_2014_1 = pd.read_csv("input/201402_babs_open_data/201402_weather_data.csv")
+weather_2014_2 = pd.read_csv("input/201408_babs_open_data/201408_weather_data.csv")
+weather_2015   = pd.read_csv("input/2015/201508_weather_data.csv")
+
+weather_columns = ["date", "max_temperature_f", "mean_temperature_f", "min_temperature_f", "max_dew_point_f", "mean_dew_point_f", "min_dew_point_f", "max_humidity", "mean_humidity", "min_humidity", "max_sea_level_pressure_inches", "mean_sea_level_pressure_inches", "min_sea_level_pressure_inches", "max_visibility_miles", "mean_visibility_miles", "min_visibility_miles", "max_wind_Speed_mph", "mean_wind_speed_mph", "max_gust_speed_mph", "precipitation_inches", "cloud_cover", "events", "wind_dir_degrees", "zip_code"]
+weather_2014_1.columns = weather_columns
+weather_2014_2.columns = weather_columns
+weather_2015.columns   = weather_columns
+
+weather = pd.concat([weather_2014_1, weather_2014_2, weather_2015])
+weather.to_csv("output/trip.csv", index=False)
+
+print(weather_2014_1.shape)
+print(weather_2014_2.shape)
+print(weather_2015.shape)
